edddb07f0b2e02f06c9616b1f47599ae4ab7c867
Greg <grigor-martirosyan-1999@mail.ru>
2021-03-17 16:22:13 +0400

    user details

--- src/main/java/am/basic/notificator/NotificatorApplication.java
+++ src/main/java/am/basic/notificator/NotificatorApplication.java
@@ -1,7 +1,10 @@
 package am.basic.notificator;
 
+import am.basic.notificator.crm.util.Md5Encoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class NotificatorApplication {
@@ -10,4 +13,8 @@ public class NotificatorApplication {
         SpringApplication.run(NotificatorApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new Md5Encoder();
+    }
 }
